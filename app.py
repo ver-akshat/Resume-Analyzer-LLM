d2d027973ab1d8af36d31c1728c0beb4a7689833
ver-akshat <38131697+ver-akshat@users.noreply.github.com>
2024-03-15 08:24:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-import time
-import numpy as np
-import pandas as pd
 import streamlit as st
 from streamlit_option_menu import option_menu
 from streamlit_extras.add_vertical_space import add_vertical_space
